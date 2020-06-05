3eefa918a2a0b4d1750cdd98a6124c86c84d7f69
Leandro Real <leo.real2585@gmail.com>
2020-06-05 16:25:24 -0300

    App

---
+++ backend/src/controllers/items.ts
@@ -0,0 +1,17 @@
+import { Request, Response} from 'express';
+import knex from '../database/connection';
+
+class ItemsController {
+  async index(req: Request,res: Response){
+    const items = await knex('items').select('*');
+    const serializedItems = items.map(item => {
+      return {
+        id: item.id,
+        title: item.name,
+        image_url: `http://192.168.1.39:3001/uploads/${item.image}`,
+      };
+    });
+    return res.json(serializedItems);
+  }
+}
+export default ItemsController;
---
+++ web/src/routes.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Route, BrowserRouter, Switch } from 'react-router-dom';
+
+import Home from './pages/Home';
+import CreatePoint from './pages/CreatePoint';
+
+const Routes = () => {
+  return(
+    <BrowserRouter>
+      <Route exact component={Home} path="/" />
+      <Route component={CreatePoint} path="/createPoint" />
+    </BrowserRouter>
+  );
+}
+export default Routes;
